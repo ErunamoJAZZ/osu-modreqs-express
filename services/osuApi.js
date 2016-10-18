727c0776e06031fa9ed7ee0db810a4faa49892a2
C. Daniel Sanchez R <cdsanchezr@unal.edu.co>
2016-10-17 21:52:15 -0500

    orz,

--- services/osuApi.js
+++ services/osuApi.js
@@ -74,7 +74,8 @@ function modRequetPlz(nick, bm_type, bm_id) {
                 request
                     .get(query_base)
                     .query({s: res.body[0].beatmapset_id})
-                    .end(function (new_res) {
+                    .end(function (err, new_res) {
+                        console.log(new_res.body);
                         insertMapAndMod(new_res.body, nick);
                     });
             }
